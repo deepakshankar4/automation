1c0091345cd0b8d71d13009469056a7061711c52
Deepak R Shankar <shankar.deepak22@hotmail.com>
2014-03-05 15:19:15 +0530

    exception class added

---
+++ automation/src/org/drait/source/exception/AutomationTransactionException.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.drait.source.exception;
+
+import org.springframework.transaction.TransactionException;
+
+/**
+ * @author UC167163
+ *
+ */
+public class AutomationTransactionException extends TransactionException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1851557973083054240L;
+
+	public AutomationTransactionException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
